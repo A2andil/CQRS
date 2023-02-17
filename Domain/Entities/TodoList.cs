3f6e9385ac41da9b6f1909c54951dba23747db3c
ahmadkandil <A77mad.22andil@gmail.com>
2023-02-18 01:05:10 +0200

    Common

---
+++ Domain/Entities/TodoList.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using Domain.Common;
+using Domain.Enums;
+using Domain.ValueObjects;
+
+namespace Domain.Entities;
+
+public class TodoList : AuditBaseEntity
+{
+    public string? Title { get; set; }
+    public Colour Colour { get; set; } = Colour.White;
+
+    public IList<TodoItem> Items { get; private set; } = new List<TodoItem>();
+}
