770bf5c3c0ecb360574ce4f725692c0bf2d911d5
ahmadkandil <A77mad.22andil@gmail.com>
2023-02-18 18:26:19 +0200

    Common in Application

--- Application/Common/Interfaces/IIdentityService.cs
+++ Application/Common/Interfaces/IIdentityService.cs
@@ -14,4 +14,3 @@ public interface IIdentityService
 
     Task<Result> DeleteUserAsync(string userId);
 }
-
---
+++ Application/Common/Mapping/MappingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Mapping;
+
+public static class MappingExtensions
+{
+    public static async Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
+       => await PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+
+    public static async Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration) where TDestination : class
+        => await queryable.ProjectTo<TDestination>(configuration).AsNoTracking().ToListAsync();
+}
+
