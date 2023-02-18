83497186044f248dc5a0a329dccace8e25fba97b
ahmadkandil <A77mad.22andil@gmail.com>
2023-02-19 01:16:52 +0200

    Features

---
+++ Application/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.TodoItems.Commands.CreateTodoItem;
+
+public record CreateTodoItemCommand : IRequest<int>
+{
+    public int ListId { get; init; }
+
+    public string? Title { get; init; }
+}
+
---
+++ Application/TodoItems/Commands/UpdateTodoItem/UpdateTodoItemCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Application.TodoItems.Commands.UpdateTodoItem;
+
+public record UpdateTodoItemCommand : IRequest
+{
+    public int Id { get; init; }
+
+    public string? Title { get; init; }
+
+    public bool Done { get; init; }
+}
+
