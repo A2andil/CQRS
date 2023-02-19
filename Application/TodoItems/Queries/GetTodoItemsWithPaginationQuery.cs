58cfa46b851672bdbd22ccea777436cbeaba1e53
ahmadkandil <A77mad.22andil@gmail.com>
2023-02-19 18:09:26 +0200

    Queries

---
+++ Application/TodoItems/Queries/GetTodoItemsWithPaginationQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.TodoItems.Queries;
+
+public class GetTodoItemsWithPaginationQuery : IRequest<PaginatedList<TodoItemBriefDto>>
+{
+    public int ListId { get; init; }
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
---
+++ Application/TodoItems/Queries/TodoItemBriefDto.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Mapping;
+using Domain.Entities;
+
+namespace Application.TodoItems.Queries;
+
+public class TodoItemBriefDto : IMapFrom<TodoItem>
+{
+    public int Id { get; set; }
+
+    public int ListId { get; set; }
+
+    public string? Title { get; set; }
+
+    public bool Done { get; set; }
+}
+
